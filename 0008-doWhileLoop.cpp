52636335a9f25ad4398f9f06c13ba1084f5521b4
dear-mahmud-bd <dearmahmud.bd@gmail.com>
2022-06-21 06:58:18 +0600

    do While loop

---
+++ 0008-doWhileLoop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cin >> num;
+    do
+    {
+        cout << num << endl;
+        cin >> num;
+    } while (num < 0);
+
+    return 0;
+}
