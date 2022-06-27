972f1771b95c862f09b769718b2b4a0dd8032f3c
dear-mahmud-bd <dearmahmud.bd@gmail.com>
2022-06-27 22:30:26 +0600

    half piramid

--- 0015-Patern.cpp
+++ 0015-Patern.cpp
@@ -2,40 +2,52 @@
 using namespace std;
 int main()
 {
-    int col, row, i, j;
-    cout << "Enter Coloum and Row : " << endl;
-    cin >> col >> row;
-    for (i = 1; i <= row; i++)
-    {
-        for (j = 1; j <= col; j++)
-        {
-            cout << "* ";
-        }
-        cout << endl;
-    }
+    // int col, row, i, j;
+    // cout << "Enter Coloum and Row : " << endl;
+    // cin >> col >> row;
+    // for (i = 1; i <= row; i++)
+    // {
+    //     for (j = 1; j <= col; j++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
 
-    int col1, row1, k, l;
-    cout << "Enter Coloum and Row : " << endl;
-    cin >> col1 >> row1;
-    for (k = 1; k <= row1; k++)
+    // int col1, row1, k, l;
+    // cout << "Enter Coloum and Row : " << endl;
+    // cin >> col1 >> row1;
+    // for (k = 1; k <= row1; k++)
+    // {
+    //     for (l = 1; l <= col1; l++)
+    //     {
+    //         if (k == 1 || k == row1 || l == 1 || l == col1)
+    //         {
+    //             cout << "* ";
+    //         }
+    //         else
+    //         {
+    //             cout << "  ";
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+
+    int num,n,m;
+    cout << "Enter an Integer : " << endl;
+    cin >> num;
+    cout<<endl;    
+    for ( m = 1; m <= num; m++)
     {
-        for (l = 1; l <= col1; l++)
+        for ( n = 1; n <= m; n++)
         {
-            if (k == 1 || k == row1)
-            {
-                cout << "* ";
-            }
-            else if (l == 1 || l == col1)
-            {
-                cout << "* ";
-            }
-            else
-            {
-                cout << "  ";
-            }
+            cout<<"* ";
         }
-        cout << endl;
+        cout<<endl;
     }
+    
+
+
 
     return 0;
 }
