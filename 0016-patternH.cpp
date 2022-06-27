8960fe034bc2edba3b384038c89eee4a7d5d216c
dear-mahmud-bd <dearmahmud.bd@gmail.com>
2022-06-28 03:08:09 +0600

    Romboses patern

--- 0016-patternH.cpp
+++ 0016-patternH.cpp
@@ -2,27 +2,45 @@
 using namespace std;
 int main()
 {
-    int num, row,col;
-    cout<<"Enter any Integer : "<<endl;
-    cin>>num;
-    for ( row = 1; row <= num; row++)
+    // int num, row,col;
+    // cout<<"Enter any Integer : "<<endl;
+    // cin>>num;
+    // for ( row = 1; row <= num; row++)
+    // {
+    //     for ( col = 1; col <= num-row; col++)
+    //     {
+    //         cout<<"  ";
+    //     }
+    //     for ( col = 1; col <= row; col++)
+    //     {
+    //         cout<<col<<" ";
+    //     }
+    //     // 2 nd part
+    //     for ( col = row-1; col >= 1; col--)
+    //     {
+    //         cout<<col<<" ";
+    //     }
+    //     cout<<endl;       
+    // }
+
+    int num1,row1,col1;
+    cout<<"Enter any Number : ";
+    cin>>num1;
+    cout<<endl;
+    for ( row1 = 1; row1 <= num1; row1++)
     {
-        for ( col = 1; col <= num-row; col++)
+        for ( col1 = 1; col1 <= num1-row1; col1++)
         {
             cout<<"  ";
         }
-        for ( col = 1; col <= row; col++)
+        for ( col1 = 1; col1 <= num1; col1++)
         {
-            cout<<col<<" ";
-        }
-        // 2 nd part
-        for ( col = row-1; col >= 1; col--)
-        {
-            cout<<col<<" ";
+            cout<<"* ";
         }
         cout<<endl;
         
     }
+    
     
     return 0;
 }
