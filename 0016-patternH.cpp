f735045337477d21c433acb6ca32491e6d00fced
dear-mahmud-bd <dearmahmud.bd@gmail.com>
2022-06-28 23:46:29 +0600

    dimond patern

--- 0016-patternH.cpp
+++ 0016-patternH.cpp
@@ -2,44 +2,44 @@
 using namespace std;
 int main()
 {
-    // Palindrom 
-    int num, row,col;
-    cout<<"Enter any Integer : "<<endl;
-    cin>>num;
-    for ( row = 1; row <= num; row++)
+    // Palindrom
+    int num, row, col;
+    cout << "Enter any Integer : " << endl;
+    cin >> num;
+    for (row = 1; row <= num; row++)
     {
-        for ( col = 1; col <= num-row; col++)
+        for (col = 1; col <= num - row; col++)
         {
-            cout<<"  ";
+            cout << "  ";
         }
-        for ( col = 1; col <= row; col++)
+        for (col = 1; col <= row; col++)
         {
-            cout<<col<<" ";
+            cout << col << " ";
         }
         // 2 nd part
-        for ( col = row-1; col >= 1; col--)
+        for (col = row - 1; col >= 1; col--)
         {
-            cout<<col<<" ";
+            cout << col << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 
     // Romboses ...
-    int num1,row1,col1;
-    cout<<"Enter any Number : ";
-    cin>>num1;
-    cout<<endl;
-    for ( row1 = 1; row1 <= num1; row1++)
+    int num1, row1, col1;
+    cout << "Enter any Number : ";
+    cin >> num1;
+    cout << endl;
+    for (row1 = 1; row1 <= num1; row1++)
     {
-        for ( col1 = 1; col1 <= num1-row1; col1++)
+        for (col1 = 1; col1 <= num1 - row1; col1++)
         {
-            cout<<"  ";
+            cout << "  ";
         }
-        for ( col1 = 1; col1 <= num1; col1++)
+        for (col1 = 1; col1 <= num1; col1++)
         {
-            cout<<"* ";
+            cout << "* ";
         }
-        cout<<endl;
+        cout << endl;
     }
 
     // dimond patern ...
@@ -47,46 +47,93 @@ int main()
     cout << "Enter any Number : ";
     cin >> num2;
     cout << endl;
-    for (row2 = 1; row2 <= num2; row2++){
-        for (col2 = 1; col2 <= num2 -row2; col2++){
-            cout<<" ";
+    for (row2 = 1; row2 <= num2; row2++)
+    {
+        for (col2 = 1; col2 <= num2 - row2; col2++)
+        {
+            cout << " ";
         }
-        for ( col2 = 1; col2 <= row2; col2++){
-            if(col2==1){
-                cout<<"*";
-            }else{
-                cout<<" ";
+        for (col2 = 1; col2 <= row2; col2++)
+        {
+            if (col2 == 1)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
             }
         }
-        for ( col2 = row2-1; col2 >= 1; col2--){
-            if(col2==1){
-                cout<<"*";
-            }else{
-                cout<<" ";
+        for (col2 = row2 - 1; col2 >= 1; col2--)
+        {
+            if (col2 == 1)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
             }
         }
-        cout<<endl;
+        cout << endl;
+    }
+    for (row2 = num2 - 1; row2 >= 1; row2--)
+    {
+        for (col2 = 1; col2 <= num2 - row2; col2++)
+        {
+            cout << " ";
+        }
+        for (col2 = 1; col2 <= row2; col2++)
+        {
+            if (col2 == 1)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        for (col2 = row2 - 1; col2 >= 1; col2--)
+        {
+            if (col2 == 1)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
     }
 
-    for ( row2 = num2-1; row2 >= 1; row2--){
-        for (col2 = 1; col2 <= num2 -row2; col2++){
-            cout<<" ";
-        }
-        for ( col2 = 1; col2 <= row2; col2++){
-            if(col2==1){
-                cout<<"*";
+    /* 
+    int num2, row2, col2;
+    cout << "Enter any Number : ";
+    cin >> num2;
+    cout << endl;
+    for (row2 = 0; row2 < num2; row2++){
+        for (col2 = 1; col2 <= num2 * 2 - 1; col2++){
+            if ((col2 == num2 - row2) || (col2 == num2 + row2)){
+                cout << " * ";
             }else{
-                cout<<" ";
+                cout << "   ";
             }
         }
-        for ( col2 = row2-1; col2 >= 1; col2--){
-            if(col2==1){
-                cout<<"*";
-            }else{
-                cout<<" ";
-            }
-        }
-        cout<<endl;
+        cout << endl;
     }
+    for (row2 = num2 - 1; row2 >= 0; row2--){
+        for (col2 = 1; col2 <= num2 * 2 - 1; col2++){
+            if ((col2 == num2 - row2) || (col2 == num2 + row2)){
+                cout << " * ";
+            }else{
+                cout << "   ";
+            }
+        }
+        cout << endl;
+    } 
+    */
+
     return 0;
 }
