185c436c495f4be822d438294647b05597b363d1
dear-mahmud-bd <dearmahmud.bd@gmail.com>
2022-06-28 22:36:50 +0600

    fibonacci

--- 0018-function.cpp
+++ 0018-function.cpp
@@ -2,6 +2,7 @@
 #include<cmath>
 using namespace std;
 
+// Check Prime number ...
 bool isPrime(int num){
     for (int i = 2; i <= sqrt(num); i++)
     {
@@ -11,8 +12,20 @@ bool isPrime(int num){
     }
     return true;
 }
-
+// fibonacci ...
+void fibonacci(int n){
+    int t1=0,t2=1,nextNum;
+    for (int i = 1; i <= n; i++)
+    {
+        cout<<t1<<" ";
+        nextNum=t1+t2;
+        t1=t2;
+        t2=nextNum;
+    }
+    return;
+}
 int main(){
+    // Check Prime number ...
     int a,b;
     cin>>a>>b;
     cout<<endl;
@@ -22,5 +35,13 @@ int main(){
             cout<<i<<endl;
         }
     }
+
+    // fibonacci
+    int nf;
+    cin>>nf;
+    fibonacci(nf);
+
+
+
     return 0;
 }
