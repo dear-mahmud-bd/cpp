683065fd63a15dbb533f26fd71085963a618703d
dear-mahmud-bd <dearmahmud.bd@gmail.com>
2022-07-13 08:20:45 +0600

    sum of sub array

--- 0027-arrayQues.cpp
+++ 0027-arrayQues.cpp
@@ -1,24 +1,43 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    for (int i = 0; i < n; i++){
-        cin>>arr[i];
-    }
-    int max=arr[0];
-    for (int i = 0; i < n; i++){
-        if (arr[i] > max){
-            max = arr[i];
-            arr[i]=max;
-        }else{
-            arr[i]=max;
-        }
-    }
-    for (int i = 0; i < n; i++){
-        cout<<arr[i]<<" ";
-    }
+
+    // // Max till i ...
+    // int n;
+    // cin>>n;
+    // int arr[n];
+    // for (int i = 0; i < n; i++){
+    //     cin>>arr[i];
+    // }
+    // int max=arr[0];
+    // for (int i = 0; i < n; i++){
+    //     if (arr[i] > max){
+    //         max = arr[i];
+    //         arr[i]=max;
+    //     }else{
+    //         arr[i]=max;
+    //     }
+    // }
+    // for (int i = 0; i < n; i++){
+    //     cout<<arr[i]<<" ";
+    // }
+
+    // // Sum of sub Array
+    // int num;
+    // cin>>num;
+    // int array[num];
+    // for (int i = 0; i < num; i++){
+    //     cin>>array[i];
+    // }
+    // int currSum = 0;
+    // for (int i = 0; i < num; i++){
+    //     currSum = 0;
+    //     for (int j = i; j < num; j++){
+    //         currSum += array[j];
+    //         cout<< currSum << " ";
+    //     }
+    // }
     
+
     return 0;
 }
