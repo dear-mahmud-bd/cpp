2c110dfa3393626e8ed94043a51a33c57e0ea3e4
dear-mahmud-bd <dearmahmud.bd@gmail.com>
2022-07-13 16:24:40 +0600

    reapiting element

---
+++ 0028-arrQues2.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // // First reapiting element ...
+    // int n1;
+    // cin>>n1;
+    // int a[n1];
+    // for (int i = 0; i < n1; i++){
+    //     cin>>a[i];
+    // }
+    // const int N = 1e6+2;
+    // int idx[N];
+    // for (int i = 0; i < N; i++){
+    //     idx[i] = -1;
+    // }
+    // int minIndex = INT_MAX;
+    // for (int i = 0; i < n1; i++){
+    //     if(idx[a[i]] != -1){
+    //         minIndex = min(minIndex, idx[a[i]]);
+    //     }else{
+    //         idx[a[i]]= i;
+    //     }
+    // }
+    // if(minIndex == INT_MAX){
+    //     cout<<"-1"<<endl;
+    // }else{
+    //     cout<<minIndex<<endl;
+    // }
+
+    
+        
+    return 0;
+}
