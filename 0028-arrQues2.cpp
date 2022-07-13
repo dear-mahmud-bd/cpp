8363e26590512ff6874ca9511f2ef6f251fad7bc
dear-mahmud-bd <dearmahmud.bd@gmail.com>
2022-07-13 16:41:05 +0600

    subArr, given sum

--- 0028-arrQues2.cpp
+++ 0028-arrQues2.cpp
@@ -27,7 +27,37 @@ int main(){
     //     cout<<minIndex<<endl;
     // }
 
-    
-        
+    // // Subarray  with given sum ...
+    // int n, s;
+    // cin>>n>>s;
+    // int ar[n];
+    // for (int i = 0; i < n; i++){
+    //     cin>>ar[i];
+    // }
+    // int i=0, j=0, st=-1, en=-1, sum = 0;
+    // while (j<n && sum+ar[j]<=s){
+    //     sum+=ar[j];
+    //     j++;
+    // }
+    // if (sum == s){
+    //     cout<<i+1<<" "<<j<<endl;
+    //     return 0;
+    // }
+    // while (j<n){
+    //     sum+=ar[j];
+    //     while (sum > s){
+    //         sum-=ar[i];
+    //         i++;
+    //     }
+    //     if(sum == s){
+    //         st = i+1;
+    //         en = j+1;
+    //         break;
+    //     }
+    //     j++;
+    // }
+    // cout<<st<<" "<<en<<endl;
+
+
     return 0;
 }
