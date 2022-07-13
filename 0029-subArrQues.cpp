d8fe5aa0765f847f5815963e011030de123720fe
dear-mahmud-bd <dearmahmud.bd@gmail.com>
2022-07-13 17:24:00 +0600

    max sub array sum

--- 0029-subArrQues.cpp
+++ 0029-subArrQues.cpp
@@ -2,19 +2,39 @@
 #include<climits>
 using namespace std;
 int main(){
-    // posible all sub array ...
-    int n1;
-    cin>>n1;
-    int arr1[n1];
-    for (int i = 0; i < n1; i++){
-        cin>>arr1[i];
+    // // posible all sub array ...
+    // int n1;
+    // cin>>n1;
+    // int arr1[n1];
+    // for (int i = 0; i < n1; i++){
+    //     cin>>arr1[i];
+    // }
+    // for (int i = 0; i < n1; i++){
+    //     for (int j = i; j < n1; j++){
+    //         for (int k = i; k <= j; k++){
+    //             cout<<arr1[k]<< " ";
+    //         }cout<<endl;
+    //     }
+    // }
+
+    // max sub array sum ...
+    int n2;
+    cin>>n2;
+    int arr2[n2];
+    for (int i = 0; i < n2; i++){
+        cin>>arr2[i];
     }
-    for (int i = 0; i < n1; i++){
-        for (int j = i; j < n1; j++){
+    int maxSum = INT_MIN;
+    for (int i = 0; i < n2; i++){
+        for (int j = i; j < n2; j++){
+            int sum = 0;
             for (int k = i; k <= j; k++){
-                cout<<arr1[k]<< " ";
-            }cout<<endl;
+                sum += arr2[k];
+            }
+            maxSum = max(maxSum,sum);
         }
     }
+    cout<<maxSum<<endl;
+
     return 0;
 }
