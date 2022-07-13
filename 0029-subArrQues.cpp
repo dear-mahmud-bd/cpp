fb04fbdf29b023f7d9e0db581a76ea1865149427
dear-mahmud-bd <dearmahmud.bd@gmail.com>
2022-07-13 17:45:26 +0600

    max sub array sum-1

--- 0029-subArrQues.cpp
+++ 0029-subArrQues.cpp
@@ -17,24 +17,47 @@ int main(){
     //     }
     // }
 
-    // max sub array sum ...
-    int n2;
-    cin>>n2;
-    int arr2[n2];
-    for (int i = 0; i < n2; i++){
-        cin>>arr2[i];
+    // // max sub array sum ...-time O(ne3)
+    // int n2;
+    // cin>>n2;
+    // int arr2[n2];
+    // for (int i = 0; i < n2; i++){
+    //     cin>>arr2[i];
+    // }
+    // int maxSum = INT_MIN;
+    // for (int i = 0; i < n2; i++){
+    //     for (int j = i; j < n2; j++){
+    //         int sum = 0;
+    //         for (int k = i; k <= j; k++){
+    //             sum += arr2[k];
+    //         }
+    //         maxSum = max(maxSum,sum);
+    //     }
+    // }
+    // cout<<maxSum<<endl;
+
+    // max sub array sum ...-time O(n+ne2)
+    int n3;
+    cin>>n3;
+    int arr3[n3];
+    for (int i = 0; i < n3; i++){
+        cin>>arr3[i];
+    }
+    int currSum[n3+1];
+    currSum[0] = 0;
+    for (int i = 1; i <= n3; i++){
+        currSum[i] = currSum[i-1] + arr3[i-1];
     }
     int maxSum = INT_MIN;
-    for (int i = 0; i < n2; i++){
-        for (int j = i; j < n2; j++){
-            int sum = 0;
-            for (int k = i; k <= j; k++){
-                sum += arr2[k];
-            }
+    for (int i = 1; i <= n3; i++){
+        int sum = 0;
+        for (int j = 0; j < i; j++){
+            sum = currSum[i] - currSum[j];
             maxSum = max(maxSum,sum);
         }
     }
     cout<<maxSum<<endl;
+    
 
     return 0;
 }
