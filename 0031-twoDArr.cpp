e95319e64227d62af5ce6c93ce3dc3286b15e179
dear-mahmud-bd <dearmahmud.bd@gmail.com>
2022-07-17 12:49:24 +0600

    find element in 2D array

--- 0031-twoDArr.cpp
+++ 0031-twoDArr.cpp
@@ -18,5 +18,23 @@ int main(){
         cout<<endl;
     }
 
+    // Found an element ...
+    int x;
+    cin>>x;
+    bool value=false;
+    for (int i = 0; i < a; i++){
+        for (int j = 0; j < b; j++){
+            if(arr[i][j]==x){
+                cout<<i+1<<" "<<j+1<<endl;
+                value = true;
+            }
+        }
+    }
+    if (value){
+        cout<<"Element is found"<<endl;
+    }else{
+        cout<<"Element is not found"<<endl;
+    }
+
     return 0;
 }
